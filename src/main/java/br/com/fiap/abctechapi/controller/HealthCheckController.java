ea68b178b873e6ba6f1328cc11fee2220844f702
Mateus Morais <mateus.batista010@gmail.com>
2022-12-13 10:47:47 -0300

    added healthcheck and renamed app.yml

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -1,5 +1,9 @@
 package br.com.fiap.abctechapi.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,4 +17,15 @@ public class HealthCheckController {
     public ResponseEntity<Object> status() {
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("version")
+    public ResponseEntity<String> version() throws IOException {
+        Properties properties = new Properties();
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+        properties.load(inputStream);
+
+        return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
+    }
+
 }
