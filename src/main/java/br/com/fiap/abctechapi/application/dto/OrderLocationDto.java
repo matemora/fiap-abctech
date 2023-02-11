7c7b67946e5fdbf78f337e198c4f4a22c40f048a
Mateus <mateus.batista010@gmail.com>
2023-02-11 20:27:26 +0000

    fixes on order creation

--- src/main/java/br/com/fiap/abctechapi/application/dto/OrderLocationDto.java
+++ src/main/java/br/com/fiap/abctechapi/application/dto/OrderLocationDto.java
@@ -2,15 +2,15 @@ package br.com.fiap.abctechapi.application.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrderLocationDto {
     @NotNull
     private Double latitude;
